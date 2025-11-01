4cd9015202358c242c01a49ea39c3bf3a2d40452
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2025-11-01 16:08:10 +0600

    "Shared Preference added"

--- lib/note_view_model.dart
+++ lib/note_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NoteViewModel extends ChangeNotifier{
 
@@ -6,19 +7,37 @@ class NoteViewModel extends ChangeNotifier{
 
   List<String> get notes => List.unmodifiable(_notes);
 
+  NoteViewModel(){
+    _loadNotes();
+  }
+  Future<void> _loadNotes() async{
+   final prefs = await SharedPreferences.getInstance();
+   final saveNotes = prefs.getStringList('notes') ?? [];
+   _notes.clear();
+   _notes.addAll(saveNotes);
+   notifyListeners();
+  }
+  Future<void> _saveNotes() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('notes', _notes);
+  }
+
   void addNote(String text){
     if(text.trim().isNotEmpty){
       _notes.add(text);
+      _saveNotes();
       notifyListeners();
     }
   }
   void deleteNoteAt(int index){
     _notes.removeAt(index);
+    _saveNotes();
     notifyListeners();
   }
   void updateNoteAt(int index,String newText){
     if(newText.trim().isNotEmpty){
       _notes[index] = newText;
+      _saveNotes();
       notifyListeners();
     }
   }
